2e5f9eb0669225299bd84fd9223010c5bee23afa
yellyoshua <usuario1529yoshua@hotmail.com>
2022-07-06 23:41:39 -0500

    add show command

--- console.py
+++ console.py
@@ -6,14 +6,30 @@ Setup console application
 """
 
 
+classes = {"Amenity", "City", "Place", "Review", "State", "User"}
+
 class Airbnb_Shell(cmd.Cmd):
     """.editorconfig"""
+    prompt = '(hbnb) '
 
     def do_create(self, arg):
         print("create")
 
     def do_show(self, arg):
-        print("show")
+        splitted_args = arg.split()
+        current_class = None
+        current_id = None
+
+        if len(splitted_args) >= 1:
+            current_class = splitted_args[0]
+        if len(splitted_args) >= 2:
+            current_id = splitted_args[1]
+        if current_class is None:
+            print("** class name missing **")
+        elif current_class not in classes:
+            print("** class name missing **")
+        elif current_id is None:
+            print("** instance id missing **")
 
     def do_destroy(self, arg):
         print("destroy")
@@ -25,6 +41,7 @@ class Airbnb_Shell(cmd.Cmd):
         print("update")
 
     def do_quit(self, arg):
+        'Command to quit the program'
         return True
 
 
