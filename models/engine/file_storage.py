564681ef53019a2aacbbc93dfe0a89cc45d45581
Mpqg <4534@holbertonschool.com>
2022-07-07 01:36:49 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
-
+import json
 """
 File storage crud
 """
+
+
+class FileStorage:
+    """Class that store the files"""
+    __file_path = "airbnb_database.json"
+    __objects = dict()
+
+    def all(self):
+        return (self.__objects)
+
+    def new(self, obj):
+        name_model_class = obj.__class__.__name__
+        self.__objects["{}.{}".format(name_model_class, obj.id)] = obj
+
+    def save(self):
+        fd = open(self.__file_path, "w")
+        json.dump(self.__objects, fd)
+
+    def reload(self):
+        
